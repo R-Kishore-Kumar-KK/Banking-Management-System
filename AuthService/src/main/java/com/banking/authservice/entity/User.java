99434aaffa39f4db1b5f386372c659f2fa9dd9b5
Kishore Kumar R <150365994+R-Kishore-Kumar-KK@users.noreply.github.com>
2026-07-19 23:03:32 +0530

    Create User entity and Role enum for Auth Service

---
+++ AuthService/src/main/java/com/banking/authservice/entity/User.java
@@ -0,0 +1,67 @@
+package com.banking.authservice.entity;
+
+import java.time.LocalDateTime;
+
+import com.banking.authservice.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false)
+	private String firstName;
+	
+	@Column(nullable = false)
+	private String lastName;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false, unique = true)
+	private String mobileNumber;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+	
+	@Column(nullable = false)
+	private boolean active;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+	
+	@PrePersist
+	public void onCreate() {
+		createdAt = LocalDateTime.now();
+		updatedAt = LocalDateTime.now();
+	}
+	
+	@PreUpdate
+	public void onUpdate() {
+		updatedAt = LocalDateTime.now();
+	}
+	
+}
---
+++ AuthService/src/main/java/com/banking/authservice/enums/Role.java
@@ -0,0 +1,7 @@
+package com.banking.authservice.enums;
+
+public enum Role {
+
+	ADMIN,
+	CUSTOMER
+}
